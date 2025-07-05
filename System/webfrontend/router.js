010dff857004bba220412816bb649c1522c11689
Quan Zhou <zhouquan2k@gmail.com>
2025-07-05 16:32:23 -0400

    Refactor：System frontend refactor with UIFramework store/router

--- System/webfrontend/router.js
+++ System/webfrontend/router.js
@@ -13,28 +13,32 @@ const Routes = [{
             path: 'user',
             meta: { title: '用户' },
             permission: 'user',
-            component: (resolve) => require(['@sys/user'], resolve)
+            component: (resolve) => require(['@sys/view/user'], resolve)
         },
         {
             name: '角色',
             path: 'role',
             meta: { title: '角色' },
             permission: 'role',
-            component: (resolve) => require(['@sys/role'], resolve)
+            component: (resolve) => require(['@sys/view/role'], resolve)
         },
         {
             name: '操作日志',
             path: 'operatelog',
             meta: { title: '操作日志' },
             permission: 'log',
-            component: (resolve) => require(['@sys/operate_log'], resolve)
+            component: (resolve) => require(['@sys/view/operate_log'], resolve)
         },
         {
             name: '工作台',
             path: 'home',
             hidden: true,
-            component: (resolve) => require(['@sys/desktop'], resolve)
-        }
+            component: (resolve) => require(['@sys/view/desktop'], resolve)
+        },
+        {
+            path: '/profile',
+            component: (resolve) => require(['@sys/view/Profile'], resolve),
+        },
     ]
 }];
 
---
+++ System/webfrontend/store/system.js
@@ -0,0 +1,140 @@
+import { login, logout, getInfo, getVersion } from '@sys/view/security_api.js';
+import { getAccessToken, setToken, removeToken, getRefreshToken } from '@/utils/auth'
+
+const system = {
+  state: {
+    id: 0, // 用户编号
+    name: '', //login name
+    nickname: '',//username
+    avatar: '',
+    roles: [],
+    permissions: null,
+    version: '',
+  },
+
+  mutations: {
+    SET_DEPTID: (state, deptId) => {
+      state.deptId = deptId
+    },
+    SET_ID: (state, id) => {
+      state.id = id
+    },
+    SET_NAME: (state, name) => {
+      state.name = name
+    },
+    SET_NICKNAME: (state, nickname) => {
+      state.nickname = nickname
+    },
+    SET_AVATAR: (state, avatar) => {
+      state.avatar = avatar
+    },
+    SET_ROLES: (state, roles) => {
+      state.roles = roles
+    },
+    SET_PERMISSIONS: (state, permissions) => {
+      state.permissions = permissions
+    },
+    SET_VERSION: (state, version) => {
+      state.version = version;
+    }
+  },
+
+  actions: {
+    // 登录
+    Login({ commit }, userInfo) {
+      return new Promise((resolve, reject) => {
+        login(userInfo).then(user => {
+          // 设置 token
+          setToken({ accessToken: user.token });
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    // 获取用户信息
+    GetInfo({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        getInfo().then(user => {
+          commit('SET_ROLES', user.roles)
+          commit('SET_PERMISSIONS', user.permissions)
+
+          commit('SET_DEPTID', user.deptId)
+          commit('SET_ID', user.userId)
+          commit('SET_NAME', user.loginName)
+          commit('SET_NICKNAME', user.username)
+          commit('SET_AVATAR', user.avatar)
+          resolve(user)
+        }).catch(error => {
+          // reject(error)
+        })
+      })
+    },
+    GetVersion({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        getVersion().then(version => {
+          commit('SET_VERSION', version)
+          resolve(version)
+        }).catch(error => {
+          // reject(error)
+        })
+      })
+    },
+
+    /*
+    // 社交登录
+    SocialLogin({ commit }, userInfo) {
+      const code = userInfo.code
+      const state = userInfo.state
+      const type = userInfo.type
+      return new Promise((resolve, reject) => {
+        socialLogin(type, code, state).then(res => {
+          res = res.data;
+          // 设置 token
+          setToken(res)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+
+    // 短信登录
+    SmsLogin({ commit }, userInfo) {
+      const mobile = userInfo.mobile.trim()
+      const mobileCode = userInfo.mobileCode
+      return new Promise((resolve, reject) => {
+        smsLogin(mobile, mobileCode).then(res => {
+          res = res.data;
+          // 设置 token
+          setToken(res)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    */
+
+    // 退出系统
+    LogOut({ commit, state }) {
+      return new Promise((resolve, reject) => {
+        logout(state.token).then(() => {
+          commit('SET_ROLES', [])
+          commit('SET_PERMISSIONS', [])
+          removeToken()
+          resolve()
+        }).catch(error => {
+          commit('SET_ROLES', [])
+          commit('SET_PERMISSIONS', [])
+          removeToken()
+          resolve()
+          // removeToken()
+          //reject(error)
+        })
+      })
+    }
+  }
+}
+
+export default system
