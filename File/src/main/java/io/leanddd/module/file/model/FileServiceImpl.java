8b133dc9c09d9223167acca7b0c0c6d058642f4a
Quan Zhou <zhouquan2k@gmail.com>
2025-11-29 15:31:47 -0500

    Enhanced: set cache for file download

--- File/src/main/java/io/leanddd/module/file/model/FileServiceImpl.java
+++ File/src/main/java/io/leanddd/module/file/model/FileServiceImpl.java
@@ -15,6 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Named
 @Service(type = Type.Command, name = "file")
@@ -22,6 +25,7 @@ import java.net.URLEncoder;
 public class FileServiceImpl implements FileService {
     
     private final FileManagerSpi fileManager;
+    private static final long DEFAULT_CACHE_SECONDS = 30L * 24 * 60 * 60;
 
     @Override
     @Command(logParam = false)
@@ -36,6 +40,7 @@ public class FileServiceImpl implements FileService {
     public void viewFile(String id, HttpServletRequest request, HttpServletResponse response) {
         var fileMeta = fileManager.getMeta(id);
         response.setContentType(fileMeta.getMimeType());
+        applyCacheHeaders(response);
         fileManager.stream(fileMeta, request, response);
     }
 
@@ -45,6 +50,7 @@ public class FileServiceImpl implements FileService {
         response.setContentType(fileMeta.getMimeType());
         response.setHeader("Content-Disposition",
                 "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
+        applyCacheHeaders(response);
         fileManager.stream(fileMeta, request, response);
     }
 
@@ -56,6 +62,7 @@ public class FileServiceImpl implements FileService {
             response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
             response.setHeader("Content-Disposition",
                     "attachment;filename=" + URLEncoder.encode(fileMeta.getFileName(), "UTF-8"));
+            applyCacheHeaders(response);
             fileManager.stream(fileMeta, request, response);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -65,5 +72,11 @@ public class FileServiceImpl implements FileService {
     @Override
     public void deleteFile(String id) {
         fileManager.delete(id);
+    }
+
+    private void applyCacheHeaders(HttpServletResponse response) {
+        response.setHeader("Cache-Control", "public, max-age=" + DEFAULT_CACHE_SECONDS);
+        response.setHeader("Expires", DateTimeFormatter.RFC_1123_DATE_TIME
+                .format(ZonedDateTime.now(ZoneOffset.UTC).plusSeconds(DEFAULT_CACHE_SECONDS)));
     }
 }
